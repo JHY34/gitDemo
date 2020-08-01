9310f5a11b0c483df1a1329889955ec086b7835e
JHY34 <hamityilmaz34@gmail.com>
2020-08-01 13:13:26 -0500

    Satrday Try3

--- src/Happy_B20.java
+++ src/Happy_B20.java
@@ -1,5 +1,10 @@
 public class Happy_B20 {
     public static void main(String[] args) {
         System.out.println("B20 is Happy");
+
+        System.out.println("Hello");
+
+
+        //
     }
 }
